26c6cbc8c05e4b87d5a6e3ec9cbdd2860022fffd
aleskruba <74367962+aleskruba@users.noreply.github.com>
2024-07-23 09:43:09 +0200

    Update cloudinary.inc.php

--- env/cloudinary.inc.php
+++ env/cloudinary.inc.php
@@ -1,6 +1,6 @@
 <?php
 // Cloudinary credentials
-define('CLOUDINARY_CLOUD_NAME', 'dzxwsgl1r');
-define('CLOUDINARY_API_KEY', '546161852865324');
-define('CLOUDINARY_API_SECRET', 'y_U0NyH8WezZL86XTq29v92QiNA');
+define('CLOUDINARY_CLOUD_NAME,'');
+define('CLOUDINARY_API_KEY', '');
+define('CLOUDINARY_API_SECRET', '');
 ?>
